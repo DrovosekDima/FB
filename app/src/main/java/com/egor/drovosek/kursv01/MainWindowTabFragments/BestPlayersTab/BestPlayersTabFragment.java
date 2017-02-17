2f6b5aa6de031b9f203977bf53ae2005161dfb55
DrovosekDima <dimachy@googlemail.com>
2017-02-17 18:21:55 +0300

    Added draft version of the LoaderCallbacks.

--- app/src/main/java/com/egor/drovosek/kursv01/MainWindowTabFragments/BestPlayersTab/BestPlayersTabFragment.java
+++ app/src/main/java/com/egor/drovosek/kursv01/MainWindowTabFragments/BestPlayersTab/BestPlayersTabFragment.java
@@ -4,79 +4,139 @@ package com.egor.drovosek.kursv01.MainWindowTabFragments.BestPlayersTab;
  * Created by Drovosek on 27/01/2017.
  */
 
+import android.app.Fragment;
 import android.content.Context;
+//import android.content.CursorLoader;
+//import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
-import android.widget.SimpleCursorAdapter;
+//import android.widget.SimpleCursorAdapter;
+//import android.app.LoaderManager.LoaderCallbacks;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+
 
 import com.egor.drovosek.kursv01.DB.FootballDBHelper;
 import com.egor.drovosek.kursv01.DB.Schema;
+import com.egor.drovosek.kursv01.MainActivity;
 import com.egor.drovosek.kursv01.R;
 
 import static com.egor.drovosek.kursv01.MainActivity.gdSeason;
+import static com.egor.drovosek.kursv01.R.id.container;
 
 
-public class BestPlayersTabFragment extends Fragment {
+public class BestPlayersTabFragment extends Fragment implements LoaderCallbacks<Cursor> {
+
+    FootballDBHelper mDB;
+    Context context;
+    ListView lvData;
+    CursorAdapterWithImage scAdapter;
+
+    static class MyCursorLoader extends CursorLoader {
+
+        FootballDBHelper db;
+
+        public MyCursorLoader(Context context, FootballDBHelper db) {
+            super(context);
+            this.db = db;
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            Cursor cursor = db.getBestPlayers(MainActivity.gdSeason);
+            return cursor;
+        }
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new MyCursorLoader(context, mDB);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        scAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
 
     public class CursorAdapterWithImage extends SimpleCursorAdapter
     {
         private Cursor c;
         private Context context;
 
-        public CursorAdapterWithImage(Context context, int layout, Cursor c, String[] from, int[] to)
+        public CursorAdapterWithImage(Context context, int layout, Cursor inCursor, String[] from, int[] to)
         {
-            super(context, layout, c, from, to);
-            this.c = c;
-            this.context = context;
+            super(context, layout, inCursor, from, to);
+            c = inCursor;
+            context = context;
         }
 
         public View getView(int pos, View inView, ViewGroup parent)
         {
-            View v = inView;
+            View row = inView;
 
-            if (v == null)
+            if (row == null)
             {
                 LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = inflater.inflate(R.layout.table_bestplayers_row, null);
+                row = inflater.inflate(R.layout.table_bestplayers_row, null);
             }
-            this.c.moveToPosition(pos);
 
-            String exerciseName = this.c.getString(this.c.getColumnIndex("firstName"));
-            String exerciseDescr = this.c.getString(this.c.getColumnIndex("secondName"));
-            byte[] image = this.c.getBlob(this.c.getColumnIndex("logo"));
+            c.moveToPosition(pos);
 
-            ImageView iv = (ImageView) v.findViewById(R.id.colLogoBP);
-            if (image != null)
-            {
-                // If there is no image in the database "NA" is stored instead of a blob
-                // test if there more than 3 chars "NA" + a terminating char if more than
-                // there is an image otherwise load the default
-                if (image.length > 3)
-                {
-                    iv.setImageBitmap(BitmapFactory.decodeByteArray(image, 0, image.length));
-                }
-            /*else
-            {
-                iv.setImageResource(R.drawable.icon); // todo: create default icon
-            }*/
-            }
-            TextView fname = (TextView) v.findViewById(R.id.colTeamNameBP);
-            fname.setText(exerciseName);
+            TextView columnFIO;
+            ImageView columnLogo;
+            TextView columnTeamName;
+            TextView columnGoals;
+            TextView columnRank;
+            String data;
 
-            TextView fDescr = (TextView) v.findViewById(R.id.colFIOBP);
-            fDescr.setText(exerciseDescr);
-            return (v);
+            columnFIO = (TextView) row.findViewById(R.id.colFIOBP);
+            columnLogo = (ImageView) row.findViewById(R.id.colLogoBP);
+            columnTeamName = (TextView) row.findViewById(R.id.colTeamNameBP);
+            columnRank = (TextView) row.findViewById(R.id.colRankBP);
+            columnGoals = (TextView) row.findViewById(R.id.colGoalsBP);
+
+            data = String.valueOf(pos+1);
+            columnRank.setText(data);
+
+            data = c.getString(c.getColumnIndex("teamName"));
+            columnTeamName.setText(data);
+
+
+            data = c.getString(c.getColumnIndex("first_name")) + " " +
+                    c.getString(c.getColumnIndex("second_name"));
+            columnFIO.setText(data);
+
+            data = String.valueOf(c.getInt(c.getColumnIndex("numberOfGoals")));
+            columnGoals.setText(data);
+
+            byte[] byteLogo = c.getBlob(c.getColumnIndex("logo"));
+            Bitmap logo = BitmapFactory.decodeByteArray(byteLogo, 0 ,byteLogo.length);
+            columnLogo.setImageBitmap(logo);
+
+            if ((pos & 1) != 0) //odd
+                row.setBackgroundColor(Color.LTGRAY);
+
+            return (row);
         }
     }
 
@@ -84,34 +144,25 @@ public class BestPlayersTabFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("Debug", "BestPlayers::onCreate()");
-/*
----------------------------- on create------------------------------------------
-        setContentView(R.layout.activity_exercises_list);
-        ListView lvExList = (ListView) findViewById(R.id.listExercises);
-        db = new WorkoutDataBase(this);
-        mCursor = db.getAllExercises();
 
-        startManagingCursor(mCursor);
-        // Now create a new list adapter bound to the cursor.
-        BaseAdapter adapter = new CursorAdapterWithImage(this, // Context.
-                R.layout.exercise_item, // Specify the row template
-                // to use (here, two
-                // columns bound to the
-                // two retrieved cursor
-                // rows).
-                mCursor, // Pass in the cursor to bind to.
-                // Array of cursor columns to bind to.
-                new String [] {WorkoutDataBase.TE_IMAGE, WorkoutDataBase.TE_EX_NAME, WorkoutDataBase.TE_DESCRIPTION},
-                // Parallel array of which template objects to bind to those
-                // columns.
-                new int[] { R.id.tvItemExerciseImage, R.id.tvItemExcersizeName, R.id.tvItemExcersizeDescription
-                });
+        context = getActivity().getApplicationContext();
 
+        mDB = new FootballDBHelper(context);
 
-        // Bind to our new adapter.
-        lvExList.setAdapter(adapter);
-----------------------------------------------------------------------
-*/
+        // формируем столбцы сопоставления
+
+        String[] from = new String[] {  "first_name",
+                                        "logo",
+                                        "teamName",
+                                        "numberOfGoals"};
+
+        int[] to = new int[] {  R.id.colFIOBP,
+                                R.id.colLogoBP,
+                                R.id.colTeamNameBP,
+                                R.id.colGoalsBP};
+
+        // создаем адаптер и настраиваем список
+        scAdapter = new CursorAdapterWithImage(context, R.layout.tab_frag_bestplayers, null, from, to);
     }
 
     @Override
@@ -121,9 +172,14 @@ public class BestPlayersTabFragment extends Fragment {
         Log.i("Debug", "BestPlayers::onCreateView()");
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.tab_frag_bestplayers, container, false);
-        Context context = getActivity().getApplicationContext();
 
-        TableLayout table = (TableLayout) view.findViewById(R.id.bestPlayersTable);
+        lvData = (ListView) view.findViewById(R.id.lvBestPlayer);
+        lvData.setAdapter(scAdapter);
+
+        // создаем лоадер для чтения данных
+        getLoaderManager().initLoader(0, null, this);
+
+        /*TableLayout table = (TableLayout) view.findViewById(R.id.bestPlayersTable);
         View row;
         TextView columnFIO;
         ImageView columnLogo;
@@ -131,7 +187,7 @@ public class BestPlayersTabFragment extends Fragment {
         TextView columnGoals;
         TextView columnRank;
 
-        FootballDBHelper mDB = new FootballDBHelper(context);
+        mDB = new FootballDBHelper(context);
 
         Cursor curs = mDB.getBestPlayers(gdSeason);
 
@@ -188,7 +244,7 @@ public class BestPlayersTabFragment extends Fragment {
             curs.close();
         }
 
-        mDB.close();
+        mDB.close();*/
 
         return view;
     }
@@ -216,40 +272,22 @@ public class BestPlayersTabFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mDB.close();
         Log.i("Debug", "BestPlayers::onDestroy()");
     }
 
-    /**
-     * Called when the view previously created by {@link #onCreateView} has
-     * been detached from the fragment.  The next time the fragment needs
-     * to be displayed, a new view will be created.  This is called
-     * after {@link #onStop()} and before {@link #onDestroy()}.  It is called
-     * <em>regardless</em> of whether {@link #onCreateView} returned a
-     * non-null view.  Internally it is called after the view's state has
-     * been saved but before it has been removed from its parent.
-     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         Log.i("Debug", "BestPlayers::onDestroyView()");
     }
 
-    /**
-     * Called when the Fragment is no longer resumed.  This is generally
-     * tied to { Activity#onPause() Activity.onPause} of the containing
-     * Activity's lifecycle.
-     */
     @Override
     public void onPause() {
         super.onPause();
         Log.i("Debug", "BestPlayers::onPause()");
     }
 
-    /**
-     * Called when the Fragment is no longer started.  This is generally
-     * tied to {Activity#onStop() Activity.onStop} of the containing
-     * Activity's lifecycle.
-     */
     @Override
     public void onStop() {
         super.onStop();
